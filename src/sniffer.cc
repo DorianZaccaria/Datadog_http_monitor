21896305ea2249f391f6288525d11bfd04687b5f
Dorian Zaccaria <dorian.zaccaria@gmail.com>
2014-02-06 08:05:12 +0000

    sniffer is working

--- src/sniffer.cc
+++ src/sniffer.cc
@@ -2,7 +2,7 @@
 #include "options.hh"
 #include "logger.hh"
 
-Sniffer*			Sniffer::_singleton = NULL;
+Sniffer*					Sniffer::_singleton = NULL;
 
 Sniffer::Sniffer ()
 {
@@ -21,15 +21,52 @@ Sniffer::getSniffer ()
   return Sniffer::_singleton;
 }
 
+void got_packet(u_char*				args,
+		const struct pcap_pkthdr*	header,
+		const u_char*			packet)
+{
+  const struct sniff_ethernet *ethernet; /* The ethernet header */
+  const struct sniff_ip *ip; /* The IP header */
+  const struct sniff_tcp *tcp; /* The TCP header */
+  const u_char *payload; /* Packet payload */
+
+  u_int size_ip;
+  u_int size_tcp;
+
+  args = args;
+  header = header;
+
+  ethernet = (struct sniff_ethernet*)(packet);
+  ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+  size_ip = IP_HL(ip)*4;
+  if (size_ip < 20) {
+    printf("   * Invalid IP header length: %u bytes\n", size_ip);
+    return;
+  }
+  tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+  size_tcp = TH_OFF(tcp)*4;
+  if (size_tcp < 20) {
+    printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+    return;
+  }
+  payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+//  std::string str (payload);
+  Logger::log (Logger::INFO, "toto");
+  ethernet = ethernet;
+  payload = payload;
+
+}
+
 int
 Sniffer::snif ()
 {
-  struct pcap_pkthdr		header;
-  const u_char*			packet;
+  struct pcap_pkthdr				header;
+  //const u_char*					packet;
 
-  packet = pcap_next(_handle, &header);
+  pcap_loop (_handle, -1, got_packet, NULL);
   printf("Jacked a packet with length of [%d]\n", header.len);
-  printf("Jacked a packet: [%s]\n", packet);
+
+
   pcap_close(_handle);
 
   return ERR_NONE;
@@ -38,12 +75,12 @@ Sniffer::snif ()
 int
 Sniffer::init ()
 {
-  char				errbuf[PCAP_ERRBUF_SIZE];
-  struct bpf_program		fp;
-  bpf_u_int32			mask;
-  bpf_u_int32			net;
-  char dev[] = "wlan1";
-  char filter_exp[] = "port 80";
+  char						errbuf[PCAP_ERRBUF_SIZE];
+  struct bpf_program				fp;
+  bpf_u_int32					mask;
+  bpf_u_int32					net;
+  char						dev[] = "lo";
+  char						filter_exp[] = "port 80";
 /*
   _dev = pcap_lookupdev (errbuf);
   if (_dev == NULL)
@@ -59,7 +96,7 @@ Sniffer::init ()
     mask = 0;
   }
 
-  _handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  _handle = pcap_open_live(dev, 1518, 1, 100, errbuf);
   if (_handle == NULL)
   {
     Logger::log (Logger::ERROR, "Couldn't open device: ", errbuf);
